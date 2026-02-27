6c7d39f69a9e0cad0c5c4f5e822a51a8ab153aa7
✯Santamaria®Kevin <125566113+santamaria314k@users.noreply.github.com>
2026-02-27 14:24:15 -0500

    Add new reward image 'img/12.PNG' to rewards array

--- logica.js
+++ logica.js
@@ -5,7 +5,7 @@ let lastReward = null;
 
 const rewards = [
   "img/1.PNG","img/2.PNG","img/3.PNG","img/4.PNG","img/5.PNG",
-  "img/6.PNG","img/7.PNG","img/8.PNG","img/9.PNG","img/10.PNG","img/11.PNG"
+  "img/6.PNG","img/7.PNG","img/8.PNG","img/9.PNG","img/10.PNG","img/11.PNG","img/12.PNG"
 ];
 
 function startGame(){
